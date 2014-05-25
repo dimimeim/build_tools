df3eec191314edff1c2f5aaca9259d9de8a53c4b
LuK <priv.luk@gmail.com>
2014-05-25 20:06:20 +0200

    F2FS fixes

--- replaceboot.sh
+++ replaceboot.sh
@@ -15,4 +15,11 @@ cd boots_f2fs
 zip -r "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" "boot_s2vep.img"
 zip -d "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" boot.img
 printf "@ boot_$device.img\n@=boot.img\n" | zipnote -w "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip"
+
+unzip "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" "META-INF\*"
+sed -i 's/mount("ext4", "EMMC", "\/dev\/block\/mmcblk0p19", "\/system");/run_program("\/sbin\/mount", "-t", "auto", "\/dev\/block\/mmcblk0p19", "\/system");/g' META-INF/com/google/android/updater-script
+sed -i '/format("ext4", "EMMC", "\/dev\/block\/mmcblk0p19", "0", "\/system"); / d' META-INF/com/google/android/updater-script
+zip -r "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" META-INF
+
 md5sum "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" > "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip.md5sum"
+rm -rf META-INF
