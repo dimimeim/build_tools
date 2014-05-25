8e7118546a04c9c7a1cbde874f739e5ab0601205
LuK <priv.luk@gmail.com>
2014-05-25 15:18:41 +0200

    Fix paths

--- replaceboot.sh
+++ replaceboot.sh
@@ -2,7 +2,7 @@ export USE_CCACHE=1
 continue=0
 
 while [ $continue -eq "0" ]; do
-	echo "Select device: s2vep, s2ve"
+	echo "Select device: s2vep, s2ve, pyramid"
 	read device
 	if [ "$device" = "s2vep" ] || [ "$device" = "s2ve" ] || [ "$device" = "pyramid" ]; then 
 		continue=1
@@ -12,7 +12,7 @@ while [ $continue -eq "0" ]; do
 done
 
 cd boots_f2fs
-zip -r "../out/device/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" "boot_$device.img"
-zip -d "../out/device/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" boot.img
-printf "@ boot_$device.img\n@=boot.img\n" | zipnote -w "../out/device/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip"
-md5sum "../out/device/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" > "../out/device/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip.md5sum"
+zip -r "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" "boot_s2vep.img"
+zip -d "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" boot.img
+printf "@ boot_$device.img\n@=boot.img\n" | zipnote -w "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip"
+md5sum "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" > "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip.md5sum"
