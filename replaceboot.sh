4deccf3a9fe5001116eda7a5b32b5b7aba3ced00
LuK <priv.luk@gmail.com>
2014-05-27 21:39:30 +0200

    Update sources

--- replaceboot.sh
+++ replaceboot.sh
@@ -19,7 +19,24 @@ printf "@ boot_$device.img\n@=boot.img\n" | zipnote -w "../out/target/product/$d
 unzip "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" "META-INF/*"
 sed -i 's/mount("ext4", "EMMC", "\/dev\/block\/mmcblk0p19", "\/system");/run_program("\/sbin\/mount", "-t", "auto", "\/dev\/block\/mmcblk0p19", "\/system");/g' META-INF/com/google/android/updater-script
 sed -i '/format("ext4", "EMMC", "\/dev\/block\/mmcblk0p19", "0", "\/system");/ d' META-INF/com/google/android/updater-script
+
+echo 'run_program("/sbin/mount", "-t", "auto", "/dev/block/mmcblk0p19", "/system"); ' >> META-INF/com/google/android/updater-script
+echo 'package_extract_dir("system", "/system");' >> META-INF/com/google/android/updater-script
+echo 'set_perm_recursive(0, 0, 0755, 0755, "/system/etc/init.d");' >> META-INF/com/google/android/updater-script
+echo 'unmount("/system");' >> META-INF/com/google/android/updater-script
+echo 'package_extract_dir("kernel", "/tmp");' >> META-INF/com/google/android/updater-script
+echo 'set_perm(0, 0, 0777, "/tmp/dd");' >> META-INF/com/google/android/updater-script
+echo 'set_perm(0, 0, 0777, "/tmp/mkbootimg.sh");' >> META-INF/com/google/android/updater-script
+echo 'set_perm(0, 0, 0777, "/tmp/mkbootimg");' >> META-INF/com/google/android/updater-script
+echo 'set_perm(0, 0, 0777, "/tmp/unpackbootimg");' >> META-INF/com/google/android/updater-script
+echo 'run_program("/tmp/dd", "if=/dev/block/mmcblk0p5", "of=/tmp/boot.img");' >> META-INF/com/google/android/updater-script
+echo 'run_program("/tmp/unpackbootimg", "-i", "/tmp/boot.img", "-o", "/tmp/");' >> META-INF/com/google/android/updater-script
+echo 'run_program("/tmp/mkbootimg.sh");' >> META-INF/com/google/android/updater-script
+echo 'run_program("/tmp/dd", "if=/tmp/newboot.img", "of=/dev/block/mmcblk0p5");' >> META-INF/com/google/android/updater-script
+
 zip -r "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" META-INF
+zip -r "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" kernel
+zip -r "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" system
 
 md5sum "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip" > "../out/target/product/$device/LS-KK-v3.1-$(date +%Y-%m-%d)-$device.zip.md5sum"
 rm -rf META-INF
